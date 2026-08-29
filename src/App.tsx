56805022404824cce3d374fef19b09b4ac7f5392
itzyaboied <hondaroja2015@gmail.com>
2026-08-29 12:39:56 -0400

    solved task 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import { Button } from "react-bootstrap";
 
 function App(): React.JSX.Element {
     return (
@@ -8,14 +9,40 @@ function App(): React.JSX.Element {
             <header className="App-header">
                 Hello World UD CISC275 with React Hooks and TypeScript
             </header>
+
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
-            <p>
+
+            <div className="Rectangles" style={{ backgroundColor: "red" }}>
+                <h1>Armored core 6 is a pretty good game</h1>
+
                 <p>Edward Rodriguez</p>
-                <p>Hello World</p>
-            </p>
+                <img
+                    className="big-image"
+                    src="https://4kwallpapers.com/images/wallpapers/armored-core-6-2560x1440-12763.png"
+                    alt="This is an armored core 6 poster"
+                />
+                <ul>
+                    <li>Armored core is made by the company called Fromsoft</li>
+                    <li>
+                        Fromsoft is the same company that made the dark sould
+                        franchise
+                    </li>
+                    <li>
+                        armored core 6 is very quickly accion pased and pretty
+                        enjoyable
+                    </li>
+                </ul>
+                <Button
+                    onClick={() => {
+                        console.log("Hello World!");
+                    }}
+                >
+                    Log Hello World
+                </Button>
+            </div>
         </div>
     );
 }
